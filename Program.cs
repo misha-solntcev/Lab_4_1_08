63ec1b5a7df1f6bddbde9926053bb78778f1132e
misha-solntcev <misha.solntcev@gmail.com>
2022-12-07 01:20:02 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*8. В массиве из 2n чисел найти сумму квадратов элементов 
+    с четными индексами и сумму кубов элементов 
+    с нечетными индексами. */
+
+namespace Lab_4_1_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] arr = new int[2 * n];
+            Random r = new Random();
+
+            int sum_even = 0;
+            int sum_odd = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(50);
+                if (i % 2 ==  0)
+                    sum_even += arr[i];
+                if (i % 2 != 0)
+                    sum_odd += arr[i];                
+            }
+
+            foreach (var item in arr)            
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            Console.WriteLine(sum_even);
+            Console.WriteLine(sum_odd);
+            Console.ReadKey();
+        }
+    }
+}
